75e786064043c977013fbfa2d605185a88f844de
unknown <cliyanage124@gmail.com>
2024-10-23 22:40:18 +0530

    formatting changes

--- Js/index.js
+++ Js/index.js
@@ -1,31 +1,43 @@
 var texts = ["STUDENT", "SOFTWARE DEVELOPER", "PROGRAMMER"];
-  var index = 0;
-  var isDeleting = false;
+var index = 0;
+var isDeleting = false;
 
-  function typeText() {
-    var currentText = texts[index];
-    if (isDeleting) {
-      document.getElementById("typing-text").textContent = currentText.substring(0, document.getElementById("typing-text").textContent.length - 1);
-    } else {
-      document.getElementById("typing-text").textContent = currentText.substring(0, document.getElementById("typing-text").textContent.length + 1);
-    }
-
-    // var delta = 300 - Math.random() * 100; // Random delay for typing speed variation
-    var delta = 50;
-
-    if (!isDeleting && document.getElementById("typing-text").textContent === currentText) {
-      delta = 1500; // Delay after typing
-      isDeleting = true;
-    } else if (isDeleting && document.getElementById("typing-text").textContent === '') {
-      isDeleting = false;
-      index++;
-      if (index >= texts.length) {
-        index = 0;
-      }
-      delta = 300; 
-    }
-
-    setTimeout(typeText, delta);
+function typeText() {
+  var currentText = texts[index];
+  if (isDeleting) {
+    document.getElementById("typing-text").textContent = currentText.substring(
+      0,
+      document.getElementById("typing-text").textContent.length - 1
+    );
+  } else {
+    document.getElementById("typing-text").textContent = currentText.substring(
+      0,
+      document.getElementById("typing-text").textContent.length + 1
+    );
   }
 
-  typeText();
+  // var delta = 300 - Math.random() * 100; // Random delay for typing speed variation
+  var delta = 50;
+
+  if (
+    !isDeleting &&
+    document.getElementById("typing-text").textContent === currentText
+  ) {
+    delta = 1500; // Delay after typing
+    isDeleting = true;
+  } else if (
+    isDeleting &&
+    document.getElementById("typing-text").textContent === ""
+  ) {
+    isDeleting = false;
+    index++;
+    if (index >= texts.length) {
+      index = 0;
+    }
+    delta = 300;
+  }
+
+  setTimeout(typeText, delta);
+}
+
+typeText();
